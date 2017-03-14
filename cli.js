004f793170fdc367646eafdfba287ee8c910395a
Alexander Nanberg <alexander.nanberg@strateg.se>
2017-03-14 15:08:25 +0100

    Refactor structure

---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const meow = require('meow');
+const logSymbols = require('log-symbols');
+const updateNotifier = require('update-notifier');
+const ui = require('./lib/ui');
+const app = require('./');
+
+const cli = meow(`
+	Usage
+		$ strt-create-project
+`);
+
+updateNotifier({pkg: cli.pkg}).notify();
+
+Promise.resolve()
+	.then(ui)
+	.then(app)
+	.then(() => {
+		console.log(`   ✨ Done`);
+	})
+	.catch(err => {
+		console.error(`\n${logSymbols.error} ${err.message}`);
+		process.exit(1);
+	});
---
+++ index.js
@@ -0,0 +1,45 @@
+const del = require('del');
+const execa = require('execa');
+const Listr = require('listr');
+const util = require('./lib/util');
+
+module.exports = name => {
+	const tasks = new Listr([
+		{
+			title: `🚚 Fetching files`,
+			task: ctx => execa('git', ['clone', '--depth=1', 'git@github.com:strt/strt-boilerplate.git', ctx.name])
+				.catch(() => {
+					throw new Error('Git clone failed');
+				})
+		},
+		{
+			title: `🚀 Setting up project`,
+			task: ctx => {
+				process.chdir(`${ctx.name}`);
+				util.updatePkg(ctx.name);
+				del(['.git'])
+					.then(() => execa('git', ['init']))
+					.then(() => execa.shell('cp .hooks/* .git/hooks && chmod +x .git/hooks/*'));
+			}
+		},
+		{
+			title: `🔧 Installing packages`,
+			task: (ctx, task) => execa('yarn')
+				.catch(() => {
+					ctx.yarn = false;
+					task.skip('Yarn not available, get it here https://yarnpkg.com');
+				})
+		},
+		{
+			title: `🔧 Installing packages with npm`,
+			enabled: ctx => ctx.yarn === false,
+			task: () => execa('npm', ['install'])
+		},
+		{
+			title: `🗑 Cleaning up`,
+			task: () => del(['LICENSE', 'README.md', '.hooks'])
+		}
+	]);
+
+	return tasks.run({name});
+};
