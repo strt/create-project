fb4d75ae864687cda7b7336fd8534326fc2d0858
Alexander Nanberg <alexander.nanberg@strateg.se>
2017-04-05 08:43:08 +0200

    v1.2 🎉

--- cli.js
+++ cli.js
@@ -2,23 +2,39 @@
 const meow = require('meow');
 const logSymbols = require('log-symbols');
 const updateNotifier = require('update-notifier');
-const ui = require('./lib/ui');
+const pathExists = require('path-exists');
 const app = require('./');
 
 const cli = meow(`
 	Usage
-		$ strt-create-project
+		$ strt-create-project <name>
+
+	Example
+		$ strt-create-project strateg
+	\n
 `);
 
 updateNotifier({pkg: cli.pkg}).notify();
 
 Promise.resolve()
-	.then(ui)
+	.then(() => {
+		if (cli.input.length === 0) {
+			throw new Error('Specify a project name');
+		}
+
+		if (pathExists.sync(cli.input[0])) {
+			throw new Error(`A directory named \`${cli.input[0]}\` already exists`);
+		}
+
+		return {
+			name: cli.input[0]
+		};
+	})
 	.then(app)
 	.then(() => {
-		console.log(` ${logSymbols.success} ✨ Done`);
+		console.log(` ${logSymbols.success} ✨  Done`);
 	})
 	.catch(err => {
-		console.error(`\n${logSymbols.error} ${err.message}`);
+		console.error(`${logSymbols.error} ${err.message}`);
 		process.exit(1);
 	});
